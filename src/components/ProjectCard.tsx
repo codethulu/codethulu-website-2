0a4b963f39710cef0b9e337490b63591cd2a4130
codethulu <brendanbell2001@hotmail.com>
2022-01-04 16:52:10 +0000

    rounded edges for projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,45 +8,45 @@ function projectCard(props: { name: string; titleCard: boolean; description: str
 
 
     <div className='project-card'>
-      <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
 
-        {/* if (props.titleCard){
+
+      {/* if (props.titleCard){
           <div className={'title title-' + props.image}></div>;
         } else {
           <h1> {props.name} </h1>;
         } */}
 
-        {props.titleCard ? (<div className={'title title-' + props.image}></div>) : (<h1> {props.name} </h1>)}
+      {props.titleCard ? (<div className={'title title-' + props.image}></div>) : (<h1> {props.name} </h1>)}
 
 
 
 
 
-        <div className={'image image-' + props.image}></div>
-        <p> {props.description}</p>
+      <div className={'image image-' + props.image}></div>
+      <p> {props.description}</p>
 
-        {props.tags.map((e) => {
-          let tagStyle = 'tag'
-          if (e == "java" || e == "javascript") {
-            tagStyle += ' orange'
-          } else if (e == "haskell") {
-            tagStyle += ' purple'
-          } else if (e == "c") {
-            tagStyle += ' green'
-          } else if (e == "postgresql") {
-            tagStyle += ' cobalt'
-          } else if (e == "html" || e == "swift") {
-            tagStyle += ' fire'
-          } else if (e == "css" || e == "typescript") {
-            tagStyle += ' ocean'
-          } else if (e == "python") {
-            tagStyle += ' snake'
-          }
-          return (
-            <div className={tagStyle}>{e}</div>
-          );
-        })}
-      </ScrollAnimation>
+      {props.tags.map((e) => {
+        let tagStyle = 'tag'
+        if (e == "java" || e == "javascript") {
+          tagStyle += ' orange'
+        } else if (e == "haskell") {
+          tagStyle += ' purple'
+        } else if (e == "c") {
+          tagStyle += ' green'
+        } else if (e == "postgresql") {
+          tagStyle += ' cobalt'
+        } else if (e == "html" || e == "swift") {
+          tagStyle += ' fire'
+        } else if (e == "css" || e == "typescript") {
+          tagStyle += ' ocean'
+        } else if (e == "python") {
+          tagStyle += ' snake'
+        }
+        return (
+          <div className={tagStyle}>{e}</div>
+        );
+      })}
+
     </div >
 
 
